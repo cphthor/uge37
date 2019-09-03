65bd073b6e3dce012fe422ae633d8de886cfc495
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-09-03 15:54:40 +0200

    opgaver uge 37 med løsninger

--- FormatOne.java
+++ FormatOne.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 public class FormatOne {
   public static void main(String[] args) {
     Scanner myScan = new Scanner(System.in);
-
-
   
   }
 
---
+++ FormatZero.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class FormatZero {
+  public static void main(String[] args) {
+
+    Scanner myScan = new Scanner(System.in);
+    double outVar = myScan.nextFloat();
+    /*
+     * OPGAVE;
+     * Producer flg output med printf
+       123,11
+       |123,1100|
+       |123,1100|
+       |   123,1100|
+       |123,1|
+       | 123,110|
+       KurtVerner
+       7888666
+       7888666
+       7.888.666
+       KurtVerner
+       |     KurtVerner|
+       */
+    //løsning
+    System.out.printf("|%.4f|%n",outVar);
+    System.out.printf("|%2.4f|%n",outVar);
+    System.out.printf("|%11.4f|%n",outVar);
+    System.out.printf("|%4.1f|%n",outVar);
+    System.out.printf("|%8.3f|%n",outVar);
+    String outString = myScan.next();
+    int outInteger = myScan.nextInt();
+    System.out.printf("%d%n",outInteger);
+    System.out.printf("%,d%n",outInteger);
+    System.out.printf("%15s%n",outString);
+    System.out.printf("|%15s|%n",outString);
+  }
+
+}
