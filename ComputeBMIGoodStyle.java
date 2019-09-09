1659bde3eda0d0a34fa163ff2573a0ce654da87c
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-09-09 17:25:56 +0200

    opgaver uge 37 med løsninger

--- ComputeBMIGoodStyle.java
+++ ComputeBMIGoodStyle.java
@@ -1,38 +1,56 @@
 import java.util.Scanner;
 public class ComputeBMIGoodStyle {
-  /*
-   *
-   */
+  public static void main(String[] args){
+    /*
+     * FORMÅL:
+     * At få kendskab til scanner-klassen, integerdivision
+     * typecasting, order-of-precedence, at bygge metoder og returnere
+     * værdier
+     *
+     * OPGAVE:
+     * Abstrakt formuleret: At kunne gemme to tal i ét tal 
+     * konkret eksempel:
+     * 1,75 og 65,5 gemmes i hver sin del af 100000
+     * Så opgaven består i at lave en beregning på de to decimal-tal
+     * så de bliver til ét heltal - i dette eksempel til 175655
+     *
+     * Det vil være en god øvelse at løse opgaven som beskrevet i bogens kap
+     * 4.8. Dvs man bygger først en skal omkring selve den algoritmiske udfordring:
+     * At gemme to tal i ét tal. Man kan jo bygge videre på den foregående opgave hvor
+     * man istedet for de "globale" klassevariablerne benytter sig af "lokale" variabler, dvs variabler
+     * som kun er kendt af den enkelte metode.
+     *
+     */
 
-  public static int doDiag() {
-    Scanner myScan = new Scanner(System.in);
-    System.out.println("Indtast din vægt i kg (med max én decimal):");
-    double weightKg = myScan.nextDouble();
-    System.out.println("Indtast din højde i meter(med max to):");
-    double heightM = myScan.nextDouble();
-    // now compute returnval
-    int retVal = (int)(weightKg * 1000 * 10) + (int)(heightM*100);
-    return retVal;
-
+    int userRetVal=doDiag();
+    double userBMI = computeBMI(userRetVal);
+    doPrint(userBMI);
   }
 
-  public static double doMath(int totalVal) {
-    // vi modtager 755175
-    double tmpH = (totalVal%1000)/100.0 ;
-    double tmpW = totalVal/1000/10.0 ;
-   System.out.println(tmpW); 
-   System.out.println(tmpH); 
-    double BMI = tmpW/(tmpH*tmpH);
-    return BMI;
+  public static int doDiag() {
+    int heightWeight;
+    Scanner myScan = new Scanner(System.in);
+    System.out.println("Indtast din vægt i kg (ex:63,4):");
+    double kiloVægt = myScan.nextDouble();
+    System.out.println("Indtast din højde i meter(ex: 1,75):");
+    double meterHoejde = myScan.nextDouble();
+    // TODO: gemme højden i den øverste del af tallet og vægten i den nederste del
+    heightWeight= (int)(meterHoejde * 100) * 1000;
+    heightWeight = heightWeight + (int)(kiloVægt * 10); 
+    return heightWeight;
+
+  }
+  public static double computeBMI(int userVal) {
+    double height,weight;
+    //TODO: regne baglæns så den øverste og nederste del trækkes ud
+    // i to variable:
+    height = userVal / 1000.0 / 100.0;
+    weight = (userVal % 1000) / 10.0;
+    double bmi = weight / ( height * height);
+    return bmi;
   }
 
   public static void doPrint(double BMI) {
     System.out.println("BMI: " + BMI);
-  }
-
-  public static void main(String[] args){
-    int retVal = doDiag();
-    double bmi = doMath(retVal);
-    doPrint(bmi);
   }
 }
