8b59195dc4f7e38085a132070f17a877e24cffe0
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-09-09 15:38:51 +0200

    opgaver uge 37 med løsninger

--- FormatZero.java
+++ FormatZero.java
@@ -6,7 +6,11 @@ public class FormatZero {
     int outInteger = 7888666;
 
     /*
+     * FORMÅL:
+     * Øve komposition, Math og afrunding
+     *
      * OPGAVE;
+     * opgave 1)
      * Producer flg output med printf og de tre variabler
        |123,1100|
        |   123,1100|
@@ -15,8 +19,15 @@ public class FormatZero {
        7888666
        7.888.666
        |     Kurt Verner|
+     * opgave 2)
+     * kvadratroden af 675 er afrundet på 14 decimal 25.98076211353316
+     * udregn og gem kvadratroden korrekt afrundet på 2. decimal kun ved
+     * hjælp af Math og de almindelige regnefunktioner.
+     * opgave 3)
+     * Samme som i opg 2) men nu med 3 decimaler
+     *
        */
-    //løsning
+    //løsning opg 1
     System.out.printf("|%.4f|%n",outVar);
     System.out.printf("|%11.4f|%n",outVar);
     System.out.printf("|%4.1f|%n",outVar);
@@ -24,6 +35,12 @@ public class FormatZero {
     System.out.printf("%d%n",outInteger);
     System.out.printf("%,d%n",outInteger);
     System.out.printf("|%15s|%n",outString);
+    //løsning opg 2
+    double mySqrt = Math.round((Math.sqrt(675)*100))/100.0;
+    System.out.println(mySqrt);
+    //løsning opg 3
+    mySqrt = Math.round((Math.sqrt(675)*1000))/1000.0;
+    System.out.println(mySqrt);
   }
 
 }
